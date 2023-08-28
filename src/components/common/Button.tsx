243fb10d6b8aed71dfa41500a21ae08795d61bd9
이무성 <79708688+dlantjdgkgk@users.noreply.github.com>
2023-08-28 17:38:16 +0900

    [18, 버튼 및 레이팅 박스 컴포넌트 작업 ] (#3)
    * feat : design theme 설정
    
    * feat : design theme 설정
    
    * feat : 버튼 및 레이팅 박스 컴포넌트 작업
    
    * feat : children 타입 수정
    
    ---------
    
    Co-authored-by: 이무성 <museong@apthefin.com>

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,25 @@
+import { styled } from 'styled-components';
+
+interface ButtonProps {
+  children: string;
+  color: string;
+  background: string;
+  size: 'large' | 'medium';
+}
+
+const ButtonStyled = styled.button<ButtonProps>`
+  width: ${(props) => (props.size === 'medium' ? '80px' : '160px')};
+  height: ${(props) => (props.size === 'medium' ? '40px' : '48px')};
+  padding: ${(props) => (props.size === 'medium' ? '10px 12px' : '14px 12px')};
+  background: ${(props) => props.background};
+  color: ${(props) => props.color};
+  border-radius: 4px;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 20px;
+`;
+
+export default function Button({ children, ...props }: ButtonProps) {
+  return <ButtonStyled {...props}>{children}</ButtonStyled>;
+}
