c0e98d455ef4af9418d87464dec13227b82a729a
이무성 <79708688+dlantjdgkgk@users.noreply.github.com>
2023-08-28 17:35:32 +0900

    [14, design system 설정 ] (#2)
    * feat : design theme 설정
    
    * feat : design theme 설정
    
    ---------
    
    Co-authored-by: 이무성 <museong@apthefin.com>

---
+++ src/styles/constants/fonts.ts
@@ -0,0 +1,39 @@
+export type FontsTypes = typeof fonts;
+
+export const fonts = {
+  h1: {
+    fontSize: '40px',
+    lineHeight: '52px',
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: '28px',
+    lineHeight: '36px',
+    fontWeight: 'bold',
+  },
+  h3: {
+    fontSize: '20px',
+    lineHeight: '28px',
+    fontWeight: 'bold',
+  },
+  t1: {
+    fontSize: '16px',
+    lineHeight: '22px',
+    fontWeight: 'bold',
+  },
+  t2: {
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: 'regular',
+  },
+  caption: {
+    fontSize: '10px',
+    lineHeight: '14px',
+    fontWeight: 'regular',
+  },
+};
