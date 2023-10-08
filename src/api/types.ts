6fdbc0988cc6b363c36257e4fb30f4045ecaa68a
Han Yeong-woo <han@yeongwoo.dev>
2023-10-09 00:02:51 +0900

    feat: add `<Tier />` (#27)

---
+++ src/api/types.ts
@@ -0,0 +1,12 @@
+export type Tier =
+  | 'UNRANKED'
+  | 'iron'
+  | 'bronze'
+  | 'silver'
+  | 'gold'
+  | 'platinum'
+  | 'emerald'
+  | 'diamond'
+  | 'master'
+  | 'grandmaster'
+  | 'challenger';
