180e06395e63c71be4ebe97014480e4377bba6d3
Han Yeong-woo <han@yeongwoo.dev>
2023-10-10 09:40:10 +0900

    refactor: remove unused code

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -38,15 +38,6 @@ const Footer = () => (
         '© 2023 logo. logo isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.\nLeague of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.'
       }
     </p>
-    {/* <FooterWrapper>
-          {footerList.map((item) => {
-            return (
-              <FooterItem key={item.id} href={item.link}>
-                {item.name}
-              </FooterItem>
-            );
-          })}
-        </FooterWrapper> */}
   </footer>
 );
 
