668028e6c562025cf07c560f9f2f244fe5d3bcb4
Han Yeong-woo <han@yeongwoo.dev>
2023-10-10 14:00:46 +0900

    feat: add `<Select />` (#20)
    * feat: add `<Select />`
    
    * refactor: use *-of-type instead of *-child
    
    * feat: add width props to `<Select />`
    
    * fix: some style issue for `<Select />`
    
    * refactor: extract `<Option />`
    
    * refactor: extract `useSelect()`
    
    * refactor: change file name to camelCase
    
    * refactor: fix eslint warnings
    
    * feat: add background to option
    
    * feat: add `onChange` event `<Select />`
    
    * feat: remove `width` props
    
    * refactor: fix eslint warnings

---
+++ src/components/icons/Up.tsx
@@ -0,0 +1,9 @@
+import type { IconProps } from './types';
+
+export default function Up(props: IconProps) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <path d="M18 15L12 9L6 15" stroke="currentColor" strokeLinecap="round" />
+    </svg>
+  );
+}
