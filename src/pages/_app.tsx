92c819f4ec86445de8f90ed8891b97d50f884683
Han Yeong-woo <han@yeongwoo.dev>
2023-10-11 14:30:13 +0900

    feat: turn off default emotion prefixer plugin (#41)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
 import '@/styles/globals.css';
-import { ThemeProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
 
 import MainLayout from '@/components/layout/MainLayout';
 import theme from '@/styles/theme';
 
 import type { AppProps } from 'next/app';
 
+const emotionCache = createCache({
+  key: 'css',
+  stylisPlugins: [],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
