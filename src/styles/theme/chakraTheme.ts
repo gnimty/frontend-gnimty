209e8a73747398b76b2c4401d458be1e2ca9e96a
Milkyway <jun2077681@naver.com>
2023-10-12 11:41:56 +0900

    Update src/styles/theme/chakraTheme.ts
    Co-authored-by: Han Yeong-woo <han@yeongwoo.dev>

--- src/styles/theme/chakraTheme.ts
+++ src/styles/theme/chakraTheme.ts
@@ -17,23 +17,10 @@ const chakraTheme = {
   styles: {
     global: {
       html: {
-        'font-family': `'Pretendard Variable',
-    Pretendard,
-    -apple-system,
-    BlinkMacSystemFont,
-    system-ui,
-    Roboto,
-    'Helvetica Neue',
-    'Segoe UI',
-    'Apple SD Gothic Neo',
-    'Noto Sans KR',
-    'Malgun Gothic',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-    sans-serif`,
+        fontFamily:
+          "'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif",
+        backgroundColor: 'gray100',
       },
-      'background-color': '#f8f8f8',
     },
   },
 };
