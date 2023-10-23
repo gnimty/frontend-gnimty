4155b7e9500506bc8fbc2135a65559365df5772b
Han Yeong-woo <han@yeongwoo.dev>
2023-10-23 14:52:15 +0900

    refactor: extract `<OnSaleCard />`

---
+++ src/components/pages/information/OnSaleCard.tsx
@@ -0,0 +1,60 @@
+import { Box, Center, Flex, HStack, VStack } from '@chakra-ui/react';
+import Image from 'next/image';
+
+import Rp from '@/assets/images/rp.svg';
+
+import type { StackProps } from '@chakra-ui/react';
+
+interface OnSaleCardProps extends StackProps {
+  name: string;
+  imgUrl: string;
+  originRp: number;
+  discountedRp: number;
+  discountedRate: number;
+}
+
+export default function OnSaleCard(props: OnSaleCardProps) {
+  const { name, imgUrl, originRp, discountedRp, discountedRate, ...restProps } = props;
+
+  return (
+    <VStack gap="12px" {...restProps}>
+      <Box pos="relative" w="352px" h="200px">
+        <Image src={imgUrl} fill alt="" css={{ borderRadius: '8px' }} />
+        <Center
+          pos="absolute"
+          top="12px"
+          right="12px"
+          w="48px"
+          h="48px"
+          bg="rgb(222 46 57 / .6)"
+          border="1px solid"
+          borderColor="main"
+          borderRadius="9999px"
+          textStyle="t2"
+          fontWeight="bold"
+          color="white"
+        >
+          -{discountedRate}%
+        </Center>
+      </Box>
+      <HStack w="352px" px="12px" justifyContent="space-between" gap="12px">
+        <Box textStyle="t1" fontWeight="bold">
+          {name}
+        </Box>
+        <HStack gap="8px">
+          <Box textStyle="t2" fontWeight="regular" color="gray500" textDecor="line-through">
+            {originRp}
+          </Box>
+          <HStack gap="4px">
+            <Flex color="rp">
+              <Rp width={22} height={22} />
+            </Flex>
+            <Box textStyle="t1" fontWeight="bold" color="gray800">
+              {discountedRp}
+            </Box>
+          </HStack>
+        </HStack>
+      </HStack>
+    </VStack>
+  );
+}
