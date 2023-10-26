05714086f3dc4fbfd9e3ee88c3299b117eee0564
Han Yeong-woo <han@yeongwoo.dev>
2023-10-26 15:35:23 +0900

    refactor: extract mock data

--- src/components/pages/information/PatchNoteBanner.tsx
+++ src/components/pages/information/PatchNoteBanner.tsx
@@ -1,12 +1,9 @@
 import { Box, Heading, Link, VStack } from '@chakra-ui/react';
 
+import patchVersion from '@/api/mocks/patchVersion';
+
 export default function PatchNoteBanner() {
-  const data = {
-    version: '13.19.1',
-    releaseNoteUrl: 'https://www.leagueoflegends.com/ko-kr/news/game-updates/patch-13-19-notes/',
-    releaseNoteImgUrl:
-      'https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt0cfea10deca29f0e/64e7e07cf1ac3dc86df8ff19/2023-Key-Art.jpg',
-  };
+  const data = patchVersion;
 
   const majorMinorVersion = data.version.split('.').slice(0, 2).join('.');
 
