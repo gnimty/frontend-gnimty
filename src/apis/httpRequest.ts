52350f343c8b88fac595d5de61813200aef9b459
hasu <jun2077681@naver.com>
2023-11-14 15:32:08 +0900

    refactor: rename request to httpRequest

--- src/apis/httpRequest.ts
+++ src/apis/httpRequest.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
-const request = axios.create({
+const httpRequest = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
-export default request;
+export default httpRequest;
