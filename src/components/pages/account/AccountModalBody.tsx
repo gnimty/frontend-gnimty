8cc1f8837396122c8cd2b5e307b503813dff7166
hasu <jun2077681@naver.com>
2023-11-16 04:20:18 +0900

    feat: make signup success modal

--- src/components/pages/account/AccountModalBody.tsx
+++ src/components/pages/account/AccountModalBody.tsx
@@ -1,5 +1,6 @@
 import LoginModalBody from '@/components/pages/account/ModalBody/LoginModalBody';
 import SignupModalBody from '@/components/pages/account/ModalBody/SignupModalBody';
+import SignupSuccessModalBody from '@/components/pages/account/ModalBody/SignupSuccessModalBody';
 import TermsModalBody from '@/components/pages/account/ModalBody/TermsModalBody';
 import { useAccountModalPageContext } from '@/contexts/AccountModalPageContext';
 
@@ -15,6 +16,7 @@ export default function AccountModalBody() {
           <TermsModalBody terms={termsData} />
         </>
       )}
+      {currentPage.page == 'SUCCESS' && <SignupSuccessModalBody />}
     </>
   );
 }
