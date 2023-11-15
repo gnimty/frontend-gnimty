beafdb8c0e181baedf1afdb8f34e56650aa16a6c
hasu <jun2077681@naver.com>
2023-11-16 04:19:36 +0900

    feat: make terms modal

--- src/components/pages/account/AccountModalBody.tsx
+++ src/components/pages/account/AccountModalBody.tsx
@@ -1,14 +1,20 @@
 import LoginModalBody from '@/components/pages/account/ModalBody/LoginModalBody';
 import SignupModalBody from '@/components/pages/account/ModalBody/SignupModalBody';
+import TermsModalBody from '@/components/pages/account/ModalBody/TermsModalBody';
 import { useAccountModalPageContext } from '@/contexts/AccountModalPageContext';
 
 export default function AccountModalBody() {
-  const { currentPage } = useAccountModalPageContext();
+  const { currentPage, termsData } = useAccountModalPageContext();
 
   return (
     <>
       {currentPage.page == 'LOGIN' && <LoginModalBody />}
       {currentPage.page == 'SIGNUP' && <SignupModalBody />}
+      {currentPage.page == 'TERMS' && termsData && (
+        <>
+          <TermsModalBody terms={termsData} />
+        </>
+      )}
     </>
   );
 }
