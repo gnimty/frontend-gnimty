2ab6bd3b12e787d53834dc19aabf9219c65b1cdc
Han Yeong-woo <han@yeongwoo.dev>
2023-11-30 20:44:22 +0900

    feat: update data dragon version to 13.23.1

--- src/apis/constants/dataDragonVersion.ts
+++ src/apis/constants/dataDragonVersion.ts
@@ -1,6 +1,6 @@
 /**
  * 최신 버전은 다음 링크에서 확인 가능: {@link https://ddragon.leagueoflegends.com/api/versions.json}
  */
-const dataDragonVersion = '13.18.1';
+const dataDragonVersion = '13.23.1';
 
 export default dataDragonVersion;
