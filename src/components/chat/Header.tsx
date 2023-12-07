77ce18fe8fa0865d2c378c3abe0b0aae3ab4ce86
LucetTin5 <lucettin5@gmail.com>
2023-12-07 16:26:11 +0900

    fix: ExitButton으로 ExitIcon 감싸기

--- src/components/chat/Header.tsx
+++ src/components/chat/Header.tsx
@@ -1,19 +1,32 @@
 import { Box, HStack, useTheme } from '@chakra-ui/react';
 
 import ExitIcon from '@/assets/icons/system/exit.svg';
+import styled from '@emotion/styled';
+
+const ExitButton = styled.button`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
 
 interface HeaderProps {
   closeChat: () => void;
 }
 
-const Header = (props: HeaderProps) => {
+const Header = ({ closeChat }: HeaderProps) => {
   const theme = useTheme();
   return (
     <HStack w="100%" h="56px" p="16px 20px" justify="space-between">
       <Box textStyle="t1" color="gray800">
         채팅 목록
       </Box>
-      <ExitIcon width="24px" height="24px" color={theme.colors.gray800} cursor="pointer" onClick={props.closeChat} />
+      <ExitButton onClick={closeChat}>
+        <ExitIcon width="24px" height="24px" color={theme.colors.gray800} />
+      </ExitButton>
     </HStack>
   );
 };
