e4148fe0a5f3111747566f3134fe690838a77ee0
LucetTin5 <lucettin5@gmail.com>
2023-12-18 15:16:32 +0900

    feat: Chat 컴포넌트 생성 및 CurrentChat에 Chat 컴포넌트 등록

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,32 @@
+import { Box, HStack } from '@chakra-ui/react';
+import ChatLeft from '@/assets/icons/system/chatLeft.svg';
+import ChatRight from '@/assets/icons/system/chatRight.svg';
+import { ChatInfo } from './CurrentChat';
+
+function Chat({ type, message, date }: ChatInfo) {
+  return (
+    <HStack
+      p="10px 16px"
+      bgColor={type === 'incoming' ? 'gray100' : '#FDF4F5'}
+      borderRadius="8px"
+      position="relative"
+      alignSelf={type === 'incoming' ? 'start' : 'end'}
+    >
+      {type === 'incoming' && (
+        <Box position="absolute" top="20%" left="-8px">
+          <ChatLeft width="8px" height="11px" />
+        </Box>
+      )}
+      <Box textStyle="t2" color="gray800" fontWeight="400">
+        {message}
+      </Box>
+      {type === 'outgoing' && (
+        <Box position="absolute" top="20%" right="-8px">
+          <ChatRight width="8px" height="11px" />
+        </Box>
+      )}
+    </HStack>
+  );
+}
+
+export default Chat;
--- src/components/chat/CurrentChat.tsx
+++ src/components/chat/CurrentChat.tsx
@@ -1,12 +1,69 @@
-import { VStack } from '@chakra-ui/react';
+import { Box, VStack } from '@chakra-ui/react';
 
 import ChatInput from './ChatInput';
+import UserCard from './UserCard';
+import Chat from './Chat';
+
+export interface ChatInfo {
+  type: 'incoming' | 'outgoing';
+  message: string;
+  date: string;
+}
 
 function CurrentChat() {
+  const chats: ChatInfo[] = [
+    {
+      type: 'incoming',
+      message: '하이하이',
+      date: '2021-10-01',
+    },
+    {
+      type: 'outgoing',
+      message: 'ㅎㅇㅎㅇ!',
+      date: '2023-12-18',
+    },
+  ];
   return (
-    <VStack w="100%" h="100%" justify="space-between">
+    <VStack w="100%" h="100%" justify="space-between" position="relative">
       {/* if user exists */}
-
+      <UserCard
+        username="TestUserName"
+        hashtag="#KR1"
+        avatarUrl="#"
+        soloRankInfo={{
+          tier: 'challenger',
+          division: 1,
+          lp: 1000,
+          mainPosition: 'MIDDLE',
+          subPosition: 'TOP',
+        }}
+        flexRankInfo={{
+          tier: 'master',
+          division: 1,
+          lp: 200,
+          mainPosition: 'MIDDLE',
+          subPosition: 'TOP',
+        }}
+      />
+      {/* if chat exists */}
+      {/* TODO: 날짜별 혹은 '오늘'의 경우 divider 추가 */}
+      {chats.length > 0 && (
+        <VStack w="full" h="full" spacing="8px" overflowY="auto" p="0 20px">
+          {chats.map((chatInfo) => (
+            <Chat key={chatInfo.date} {...chatInfo} />
+          ))}
+        </VStack>
+      )}
+      {/* if chat not exists */}
+      {chats.length === 0 && (
+        <VStack w="full" h="full" align="center" justify="center">
+          <Box textStyle="t2" color="gray500" fontWeight="400" textAlign="center">
+            같이 플레이하고 싶은 소환사에게
+            <br />
+            메세지를 보내보세요!
+          </Box>
+        </VStack>
+      )}
       <ChatInput />
     </VStack>
   );
