c11a7a49964a1d408a78fd76fea1f5352c277b0a
LucetTin5 <lucettin5@gmail.com>
2024-02-06 17:48:21 +0900

    fix: radio inside fix

--- src/components/common/Radio.tsx
+++ src/components/common/Radio.tsx
@@ -29,9 +29,18 @@ const RadioInput = styled.input`
   border-radius: 50%;
 
   &:checked {
-    background-position: center;
-    background-repeat: no-repeat;
-    border: none;
+    border: 1px solid ${({ theme }) => theme.colors.red800};
+    &::after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 12px;
+      height: 12px;
+      background-color: ${({ theme }) => theme.colors.red800};
+      border-radius: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 `;
 
