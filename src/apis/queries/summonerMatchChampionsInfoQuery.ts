ab7759234f637461e0876e705b3cdde4db966377
LucetTin5 <lucettin5@gmail.com>
2024-02-22 15:43:27 +0900

    feat: 전적 챔피언정보 조회 쿼리

---
+++ src/apis/queries/summonerMatchChampionsInfoQuery.ts
@@ -0,0 +1,37 @@
+import { queryOptions } from '@tanstack/react-query';
+
+import httpRequest from '../httpRequest';
+
+import type { SummonerPlayDto, QueueType } from '../types';
+
+interface Options {
+  summonerTagName: string;
+  brief?: boolean;
+  queue_type?: QueueType;
+}
+
+interface SummonerMatchChampionsInfoResponse {
+  data: {
+    summonerPlays: SummonerPlayDto[];
+  };
+}
+
+const summonerMatchChampionsInfoQuery = ({ summonerTagName, brief = false, queue_type = 'ALL' }: Options) =>
+  queryOptions({
+    queryKey: ['summonerMatchChampionsInfo', summonerTagName, brief, queue_type],
+    async queryFn() {
+      const res = await httpRequest.get<SummonerMatchChampionsInfoResponse>(
+        `/statistics/summoner/champion/${summonerTagName}`,
+        {
+          params: {
+            brief,
+            queue_type,
+          },
+        },
+      );
+      return res.data;
+    },
+    enabled: !!summonerTagName,
+  });
+
+export default summonerMatchChampionsInfoQuery;
