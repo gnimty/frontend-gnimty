950bb2644a50dce4ed3294bdbb12508e3289b44b
Han Yeong-woo <han@yeongwoo.dev>
2024-03-02 20:18:36 +0900

    feat: improve styling search in main page

--- src/components/pages/main/Search.tsx
+++ src/components/pages/main/Search.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, IconButton, Input } from '@chakra-ui/react';
+import { Box, HStack, IconButton, Input, Text } from '@chakra-ui/react';
 
 import SearchIcon from '@/assets/icons/system/search.svg';
 
@@ -13,25 +13,34 @@ export default function Search(props: SearchProps) {
   };
 
   return (
-    <Flex p="12px 24px" w="420px" bg="white" borderRadius="40px" border="1px solid" borderColor="gray200" {...props}>
-      <Flex h="24px" alignItems="center" w="full">
-        <Box textStyle="t2" fontWeight={400} color="gray800">
+    <HStack
+      gap="12px"
+      p="12px 24px"
+      w="420px"
+      bg="white"
+      borderRadius="40px"
+      border="1px solid"
+      borderColor="gray200"
+      {...props}
+    >
+      <Box w="38px" h="20px">
+        <Text textStyle="t2" fontWeight="regular" color="gray800">
           KR
-        </Box>
-        <Box ml="32px" mr="12px" w="1px" h="full" bg="gray200" />
-        <Flex as="form" onSubmit={handleSubmit} flex="1 0 0" h="full" alignItems="center">
-          <Input
-            type="search"
-            css={{ '::webkit-search-cancel-button': { display: 'none' } }}
-            h="full"
-            outline="none"
-            flex="1 0 0"
-          />
-          <IconButton type="submit" aria-label="검색" ml="12px">
-            <SearchIcon width={24} height={24} />
-          </IconButton>
-        </Flex>
-      </Flex>
-    </Flex>
+        </Text>
+      </Box>
+      <Box w="1px" h="24px" bg="gray200" />
+      <HStack as="form" onSubmit={handleSubmit} alignItems="center" gap="12px" flex="1 0 0">
+        <Input
+          type="search"
+          textStyle="t2"
+          fontWeight="regular"
+          p={0}
+          css={{ '::-webkit-search-cancel-button': { display: 'none' } }}
+        />
+        <IconButton type="submit" aria-label="검색" display="inline-flex">
+          <SearchIcon width={24} height={24} />
+        </IconButton>
+      </HStack>
+    </HStack>
   );
 }
