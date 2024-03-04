660e3458f7e4fd7716372cf2a49fdae483fe643d
Han Yeong-woo <han@yeongwoo.dev>
2024-03-04 17:21:32 +0900

    refactor: extract common styles and logics

--- src/components/pages/main/search/RecentSearchesTab.tsx
+++ src/components/pages/main/search/RecentSearchesTab.tsx
@@ -1,31 +1,17 @@
-import { Center, Text, VStack } from '@chakra-ui/react';
-
-import SearchPopRow from './SearchPopRow';
+import SearchPopBody from './SearchPopBody';
 import useRecentSearchesStore from './useRecentSearchesStore';
 
 export default function RecentSearchTab() {
   const recentSearches = useRecentSearchesStore((state) => state.recentSearches);
   const removeRecentSearch = useRecentSearchesStore((state) => state.removeRecentSearch);
 
-  if (recentSearches.length === 0) {
-    return (
-      <Center h="180px">
-        <Text textStyle="t2" fontWeight="regular" color="gray500">
-          아직 검색한 내용이 없어요!
-        </Text>
-      </Center>
-    );
-  }
-
   return (
-    <VStack gap="16px" p="20px">
-      {recentSearches.map((recentSearch) => (
-        <SearchPopRow
-          key={recentSearch.puuid}
-          searchPopRowItem={recentSearch}
-          onXButtonClick={() => removeRecentSearch(recentSearch.puuid)}
-        />
-      ))}
-    </VStack>
+    <SearchPopBody
+      textWhenEmpty="아직 검색한 내용이 없어요!"
+      items={recentSearches}
+      onXButtonClick={(puuid) => {
+        removeRecentSearch(puuid);
+      }}
+    />
   );
 }
