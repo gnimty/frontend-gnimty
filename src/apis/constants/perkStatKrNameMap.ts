4e0e3a15f5a57218946ada55bddd9870021b6e58
LucetTin5 <lucettin5@gmail.com>
2024-03-06 15:48:09 +0900

    feat: 룬 파편 image 관련

---
+++ src/apis/constants/perkStatKrNameMap.ts
@@ -0,0 +1,10 @@
+const perkStatKrNameMap: Record<number, string> = {
+  5001: '체력',
+  5002: '방어력',
+  5003: '마법 저항력',
+  5005: '공격 속도',
+  5007: '스킬 가속',
+  5008: '적응형 능력치',
+};
+
+export default perkStatKrNameMap;
