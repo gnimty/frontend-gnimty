ce7afd658efcb89f4460675f76749dcb33fc857e
Han Yeong-woo <han@yeongwoo.dev>
2024-03-14 15:49:30 +0900

    feat: change route `/champion` to `/champions`

--- src/components/pages/champions/ChampionsPage.tsx
+++ src/components/pages/champions/ChampionsPage.tsx
@@ -1,6 +1,6 @@
 import { VStack } from '@chakra-ui/react';
 
-export default function RankPage() {
+export default function ChampionsPage() {
   return (
     <VStack w="1080px" m="0 auto">
       {/* 라인별 챔피언 목록 */}
