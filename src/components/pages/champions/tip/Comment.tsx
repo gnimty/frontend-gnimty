2ba5b330d23cc9b6dc47aceef29c422066fab8c3
LucetTin5 <lucettin5@gmail.com>
2024-04-16 16:48:04 +0900

    feat: delete modal

--- src/components/pages/champions/tip/Comment.tsx
+++ src/components/pages/champions/tip/Comment.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, HStack, Text, Textarea, VStack } from '@chakra-ui/react';
+import { Box, Button, Divider, HStack, Text, Textarea, useDisclosure, VStack } from '@chakra-ui/react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
@@ -15,6 +15,7 @@ import Up from '@/assets/icons/system/up.svg';
 import PositionImage from '@/components/common/position-image/PositionImage';
 import TierImage from '@/components/common/TierImage';
 
+import { DeleteModal } from './DeleteModal';
 import Replies from './Replies';
 
 dayjs.locale('ko');
@@ -27,6 +28,7 @@ interface CommentProps {
 
 export default function Comment({ comment, championId }: CommentProps) {
   const queryClient = useQueryClient();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { mutateAsync: updateCommentAsync, isSuccess: isUpdateSuccess } = useMutation({
     mutationFn: patchChampionComments,
   });
@@ -89,152 +91,155 @@ export default function Comment({ comment, championId }: CommentProps) {
   }
 
   return (
-    <VStack bgColor="white" p="20px" gap="12px" align="flex-start">
-      <HStack w="full" justify="space-between">
-        <HStack h="24px" gap="12px" align="center">
-          <Box w="24px" h="24px" overflow="hidden" borderRadius="12px">
-            <Image src={profileIconUrl(1)} width="24" height="24" alt="profileIcon" />
-          </Box>
-          <Text textStyle="t2" fontWeight="700">
-            {internalTagName}
-          </Text>
-          <HStack gap="4px" align="center">
-            <TierImage tier={tier} width="24" />
-            <Text textStyle="t2" fontWeight="400">
-              {fullTierName(tier, division)}
+    <>
+      <DeleteModal isOpen={isOpen} onClose={onClose} handleDelete={handleDelete} />
+      <VStack bgColor="white" p="20px" gap="12px" align="flex-start">
+        <HStack w="full" justify="space-between">
+          <HStack h="24px" gap="12px" align="center">
+            <Box w="24px" h="24px" overflow="hidden" borderRadius="12px">
+              <Image src={profileIconUrl(1)} width="24" height="24" alt="profileIcon" />
+            </Box>
+            <Text textStyle="t2" fontWeight="700">
+              {internalTagName}
             </Text>
-          </HStack>
-          <Divider orientation="vertical" h="full" colorScheme="gray500" />
-          <Text textStyle="body" fontWeight="400" color="gray500">
-            {/* TODO: createdAt 비교 */}
-            {dayjs(createdAt).from(dayjs())}
-          </Text>
-        </HStack>
-        {!isEdit && (
-          <HStack h="24px" gap="8px">
-            {/* TODO: 수정/삭제 권한 확인 필요 -  */}
-            {/* TODO: 수정 방식 논의 */}
-            <Text textStyle="body" fontWeight="400" color="gray500" onClick={() => setIsEdit(true)} cursor="pointer">
-              수정
-            </Text>
-            <Divider orientation="vertical" h="full" colorScheme="gray500" />
-            {/* TODO: 삭제 방식 논의 */}
-            <Text textStyle="body" fontWeight="400" color="gray500" onClick={handleDelete}>
-              삭제
-            </Text>
-          </HStack>
-        )}
-      </HStack>
-      <HStack gap="4px">
-        <Box p="4px 8px" borderRadius="999px" bgColor="main" color="white">
-          <Text textStyle="body" fontWeight="400">
-            {commentsType === 'TIP' ? '그님팁' : '알려주세요'}
-          </Text>
-        </Box>
-        <Box
-          w="24px"
-          h="24px"
-          borderRadius="999px"
-          bgColor="gray200"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <PositionImage position={lane} width="16" />
-        </Box>
-        <Box w="24px" h="24px" borderRadius="999px" overflow="hidden">
-          <Image src={championIconUrl(championName)} width="24" height="24" alt="Ezreal" />
-        </Box>
-        <Box borderRadius="999px" bgColor="gray200" color="gray600" p="4px 8px">
-          <Text textStyle="body" fontWeight="400">
-            v {version}
-          </Text>
-        </Box>
-      </HStack>
-      {isEdit ? (
-        <HStack gap="12px">
-          <Textarea
-            h="140px"
-            ref={textareaRef}
-            rows={4}
-            _placeholder={{
-              color: 'gray500',
-              textStyle: 't2',
-              fontWeight: '400',
-            }}
-            borderColor="gray400"
-            value={contents}
-          />
-          <VStack w="80px" gap="12px">
-            <Button
-              h="52px"
-              borderRadius="4px"
-              textStyle="t2"
-              fontWeight="700"
-              color="gray700"
-              border="1px solid"
-              borderColor="gray200"
-              p="16px 12px"
-              onClick={() => setIsEdit(false)}
-            >
-              취소
-            </Button>
-            <Button
-              h="76px"
-              borderRadius="4px"
-              textStyle="t2"
-              fontWeight="700"
-              color="gray700"
-              bgColor="main"
-              p="14px 12px"
-              onClick={handleUpdate}
-            >
-              저장
-            </Button>
-          </VStack>
-        </HStack>
-      ) : (
-        <Box w="full" textStyle="t2" fontWeight="400">
-          {contents}
-        </Box>
-      )}
-      <HStack w="full" justify="space-between">
-        <HStack gap="12px">
-          {childChampionComments.length > 0 && (
-            <Button display="flex" alignItems="center" justifyContent="space-between" gap="2px">
-              <Text textStyle="t2" fontWeight="400" color="gray600">
-                {childChampionComments.length}개의 답글
+            <HStack gap="4px" align="center">
+              <TierImage tier={tier} width="24" />
+              <Text textStyle="t2" fontWeight="400">
+                {fullTierName(tier, division)}
               </Text>
-              {/* TODO: replies open/close */}
-              <Up width="20" height="20" />
-            </Button>
-          )}
-          <Button borderBottom="1px solid" borderColor="gray600">
-            <Text textStyle="t2" fontWeight="400" color="gray600">
-              답글달기
+            </HStack>
+            <Divider orientation="vertical" h="full" colorScheme="gray500" />
+            <Text textStyle="body" fontWeight="400" color="gray500">
+              {/* TODO: createdAt 비교 */}
+              {dayjs(createdAt).from(dayjs())}
             </Text>
-          </Button>
-        </HStack>
-        <HStack gap="8px">
-          <HStack borderRadius="999px" p="4px 8px" bgColor="main" color="white" textStyle="body" gap="4px">
-            <Text fontWeight="400">추천</Text>
-            <Text fontWeight="700">{upCount}</Text>
           </HStack>
-          <HStack
+          {!isEdit && (
+            <HStack h="24px" gap="8px">
+              {/* TODO: 수정/삭제 권한 확인 필요 -  */}
+              {/* TODO: 수정 방식 논의 */}
+              <Text textStyle="body" fontWeight="400" color="gray500" onClick={() => setIsEdit(true)} cursor="pointer">
+                수정
+              </Text>
+              <Divider orientation="vertical" h="full" colorScheme="gray500" />
+              {/* TODO: 삭제 방식 논의 */}
+              <Text textStyle="body" fontWeight="400" color="gray500" onClick={onOpen}>
+                삭제
+              </Text>
+            </HStack>
+          )}
+        </HStack>
+        <HStack gap="4px">
+          <Box p="4px 8px" borderRadius="999px" bgColor="main" color="white">
+            <Text textStyle="body" fontWeight="400">
+              {commentsType === 'TIP' ? '그님팁' : '알려주세요'}
+            </Text>
+          </Box>
+          <Box
+            w="24px"
+            h="24px"
             borderRadius="999px"
-            p="4px 8px"
-            color="gray600"
-            border="1px solid"
-            borderColor="gray400"
-            textStyle="body"
-            gap="4px"
+            bgColor="gray200"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
           >
-            <Text fontWeight="400">비추천</Text>
-            <Text fontWeight="700">{downCount}</Text>
+            <PositionImage position={lane} width="16" />
+          </Box>
+          <Box w="24px" h="24px" borderRadius="999px" overflow="hidden">
+            <Image src={championIconUrl(championName)} width="24" height="24" alt="Ezreal" />
+          </Box>
+          <Box borderRadius="999px" bgColor="gray200" color="gray600" p="4px 8px">
+            <Text textStyle="body" fontWeight="400">
+              v {version}
+            </Text>
+          </Box>
+        </HStack>
+        {isEdit ? (
+          <HStack gap="12px">
+            <Textarea
+              h="140px"
+              ref={textareaRef}
+              rows={4}
+              _placeholder={{
+                color: 'gray500',
+                textStyle: 't2',
+                fontWeight: '400',
+              }}
+              borderColor="gray400"
+              value={contents}
+            />
+            <VStack w="80px" gap="12px">
+              <Button
+                h="52px"
+                borderRadius="4px"
+                textStyle="t2"
+                fontWeight="700"
+                color="gray700"
+                border="1px solid"
+                borderColor="gray200"
+                p="16px 12px"
+                onClick={() => setIsEdit(false)}
+              >
+                취소
+              </Button>
+              <Button
+                h="76px"
+                borderRadius="4px"
+                textStyle="t2"
+                fontWeight="700"
+                color="gray700"
+                bgColor="main"
+                p="14px 12px"
+                onClick={handleUpdate}
+              >
+                저장
+              </Button>
+            </VStack>
+          </HStack>
+        ) : (
+          <Box w="full" textStyle="t2" fontWeight="400">
+            {contents}
+          </Box>
+        )}
+        <HStack w="full" justify="space-between">
+          <HStack gap="12px">
+            {childChampionComments.length > 0 && (
+              <Button display="flex" alignItems="center" justifyContent="space-between" gap="2px">
+                <Text textStyle="t2" fontWeight="400" color="gray600">
+                  {childChampionComments.length}개의 답글
+                </Text>
+                {/* TODO: replies open/close */}
+                <Up width="20" height="20" />
+              </Button>
+            )}
+            <Button borderBottom="1px solid" borderColor="gray600">
+              <Text textStyle="t2" fontWeight="400" color="gray600">
+                답글달기
+              </Text>
+            </Button>
+          </HStack>
+          <HStack gap="8px">
+            <HStack borderRadius="999px" p="4px 8px" bgColor="main" color="white" textStyle="body" gap="4px">
+              <Text fontWeight="400">추천</Text>
+              <Text fontWeight="700">{upCount}</Text>
+            </HStack>
+            <HStack
+              borderRadius="999px"
+              p="4px 8px"
+              color="gray600"
+              border="1px solid"
+              borderColor="gray400"
+              textStyle="body"
+              gap="4px"
+            >
+              <Text fontWeight="400">비추천</Text>
+              <Text fontWeight="700">{downCount}</Text>
+            </HStack>
           </HStack>
         </HStack>
-      </HStack>
-      {childChampionComments.length > 0 && <Replies />}
-    </VStack>
+        {childChampionComments.length > 0 && <Replies />}
+      </VStack>
+    </>
   );
 }
---
+++ src/components/pages/champions/tip/DeleteModal.tsx
@@ -0,0 +1,36 @@
+import { Button, Modal, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
+
+interface DeleteModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  handleDelete: () => void;
+}
+
+export const DeleteModal = ({ isOpen, onClose, handleDelete }: DeleteModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent w="280px" h="144px" borderRadius="8px" p="24px 12px 12px 12px" gap="24px" bgColor="white">
+        <ModalHeader h="44px">
+          <Text textStyle="t1" fontWeight="700" color="gray800">
+            작성하신 글을
+            <br />
+            정말로 삭제하신겠습니까?
+          </Text>
+        </ModalHeader>
+        <ModalFooter h="40px" gap="12px">
+          <Button borderRadius="4px" border="1px solid" borderColor="gray200" p="10px 12px" onClick={onClose}>
+            <Text textStyle="t2" fontWeight="400" color="gray700">
+              취소
+            </Text>
+          </Button>
+          <Button borderRadius="4px" bgColor="gray800" p="10px 12px" onClick={handleDelete}>
+            <Text textStyle="t2" fontWeight="400" color="white">
+              확인
+            </Text>
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
