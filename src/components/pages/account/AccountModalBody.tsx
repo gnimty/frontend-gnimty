78da55d596385b80c925b0e1dec6036d175fdcbd
LucetTin5 <lucettin5@gmail.com>
2024-04-19 12:47:46 +0900

    feat: link set pw modal body

--- src/components/pages/account/AccountModalBody.tsx
+++ src/components/pages/account/AccountModalBody.tsx
@@ -1,5 +1,6 @@
 import FindPasswordModalBody from '@/components/pages/account/ModalBody/FindPasswordModalBody';
 import LoginModalBody from '@/components/pages/account/ModalBody/LoginModalBody';
+import SetPasswordModalBody from '@/components/pages/account/ModalBody/SetPasswordModalBody';
 import SignupModalBody from '@/components/pages/account/ModalBody/SignupModalBody';
 import SignupSuccessModalBody from '@/components/pages/account/ModalBody/SignupSuccessModalBody';
 import TermsModalBody from '@/components/pages/account/ModalBody/TermsModalBody';
@@ -19,6 +20,7 @@ export default function AccountModalBody() {
       )}
       {currentPage.page === 'SUCCESS' && <SignupSuccessModalBody />}
       {currentPage.page === 'FIND_PW' && <FindPasswordModalBody />}
+      {currentPage.page === 'SET_PW' && <SetPasswordModalBody />}
     </>
   );
 }
