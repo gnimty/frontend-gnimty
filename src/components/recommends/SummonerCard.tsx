9e687ca5e00350da106c05e08bd9ac1414e010ef
LucetTin5 <lucettin5@gmail.com>
2024-04-26 17:17:53 +0900

    feat: members like query 생성 및 적용

--- src/components/recommends/SummonerCard.tsx
+++ src/components/recommends/SummonerCard.tsx
@@ -1,20 +1,26 @@
 import { Box, Text, VStack, HStack, Button } from '@chakra-ui/react';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 import championIdEnNameMap from '@/apis/constants/championIdEnNameMap';
+import { postMemberLike } from '@/apis/queries/memberLikeQuery';
 import type { RecentlySummonersEntry } from '@/apis/types';
 import fullTierName from '@/apis/utils/fullTierName';
-import Like from '@/assets/icons/system/like.svg';
 
 import ChampionIcon from '../common/ChampionIcon';
 import ProfileImage from '../common/ProfileImage';
 import TierImage from '../common/TierImage';
 
+import LikeIcon from './LikeIcon';
+
+import type { AxiosError } from 'axios';
 interface SummonerCardProps {
   summoner: RecentlySummonersEntry;
 }
 
 const SummonerCard = ({ summoner }: SummonerCardProps) => {
   const {
+    memberId,
     name,
     tagLine,
     iconId,
@@ -28,6 +34,44 @@ const SummonerCard = ({ summoner }: SummonerCardProps) => {
     frequentChampionId3,
   } = summoner;
   const championIds = [frequentChampionId1, frequentChampionId2, frequentChampionId3];
+  const [memberLiked, setMemberLiked] = useState(false);
+  const { mutateAsync: memberLikeAsync } = useMutation({
+    mutationFn: postMemberLike,
+    onSuccess: () => {
+      setMemberLiked(!memberLiked);
+    },
+    onError: (error: AxiosError) => {
+      if (error.response?.status === 409) {
+        // 이미 좋아요를 누른 경우
+        // alert("이미 추천한 소환사입니다.")
+        setMemberLiked(true);
+      }
+      if (error.response?.status === 404) {
+        // 좋아요 기록이 없는 경우
+        setMemberLiked(false);
+      }
+    },
+  });
+  const handleMemberLike = async (targetMemberId: number) => {
+    if (memberLiked) {
+      // 이미 좋아요를 누른 경우
+      if (confirm('좋아요를 취소하시겠습니까?')) {
+        await memberLikeAsync({ targetMemberId, cancel: true });
+      }
+      return;
+    }
+    if (!memberLiked) {
+      await memberLikeAsync({ targetMemberId, cancel: false });
+    }
+  };
+
+  // 좋아요 상태 확인
+  useEffect(() => {
+    async () => {
+      await memberLikeAsync({ targetMemberId: memberId, cancel: false });
+    };
+  }, [memberLikeAsync, memberId]);
+
   return (
     <VStack w="320px" borderRadius="8px" bgColor="white" p="60px 20px 20px 20px" gap="40px">
       <Box w="60px" h="60px" borderRadius="30px" overflow="hidden">
@@ -56,10 +100,10 @@ const SummonerCard = ({ summoner }: SummonerCardProps) => {
         <VStack w="180px" gap="12px" p="20px 0 0 0" borderTop="1px solid" borderColor="gray300">
           <HStack gap="8px">
             <Text textStyle="h3" fontWeight="400">
-              {totalPlay && totalPlay + '게임'}
+              {totalPlay && totalPlay}게임
             </Text>
             <Text textStyle="h3" fontWeight="700" color="green800">
-              {winRate && (winRate * 100).toFixed(0) + '%'}
+              {winRate && (winRate * 100).toFixed(0)}%
             </Text>
           </HStack>
           <HStack gap="12px">
@@ -89,11 +133,12 @@ const SummonerCard = ({ summoner }: SummonerCardProps) => {
         display="flex"
         alignItems="center"
         justifyContent="center"
+        onClick={async () => handleMemberLike(memberId)}
       >
         <Text textStyle="t2" fontWeight="400">
           소환사 추천하기
         </Text>
-        <Like width="20px" height="20px" stroke="#fff" />
+        <LikeIcon width="20px" height="20px" stroke="#fff" like={memberLiked} />
       </Button>
     </VStack>
   );
