92f27aa8c0af30dc9cde23c20926279a63d85816
LucetTin5 <lucettin5@gmail.com>
2024-04-29 15:39:40 +0900

    fix: recommend modal 비로그인시 비활성화

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -93,7 +93,10 @@ export default function Header() {
         )}
       </header>
       <AccountModal />
-      <RecommendsModal isOpen={isOpenRecommends} onClose={onCloseRecommends} />
+      {/* TODO: summonerSearchBar 활성 시 제거 */}
+      {isAuthenticated && myInfoData !== undefined && (
+        <RecommendsModal isOpen={isOpenRecommends} onClose={onCloseRecommends} />
+      )}
     </>
   );
 }
