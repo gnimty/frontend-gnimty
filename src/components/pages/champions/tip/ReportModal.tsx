da57fb98249f372c697f57da1864a88ee9fe95bd
LucetTin5 <lucettin5@gmail.com>
2024-05-02 17:02:55 +0900

    feat: report modal style, report type setting

--- src/components/pages/champions/tip/ReportModal.tsx
+++ src/components/pages/champions/tip/ReportModal.tsx
@@ -12,7 +12,7 @@ import {
   Textarea,
   VStack,
 } from '@chakra-ui/react';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 import Exit from '@/assets/icons/system/exit.svg';
 
@@ -23,8 +23,15 @@ interface ReportModalProps {
 }
 
 export const ReportModal = ({ isOpen, onClose, handleReport }: ReportModalProps) => {
-  const [optionType, setOptionType] = useState<'ABUSE' | 'OTHER'>();
-  const reportCommentRef = useRef<HTMLTextAreaElement>(null);
+  const [optionType, setOptionType] = useState<'ABUSE' | 'OTHER' | null>(null);
+  const [reportComment, setReportComment] = useState('');
+  const buttonActive = (() => {
+    if (optionType === null) return false;
+    if (optionType === 'ABUSE') return true;
+    if (optionType === 'OTHER' && reportComment.length > 0) return true;
+    return false;
+  })();
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -40,56 +47,56 @@ export const ReportModal = ({ isOpen, onClose, handleReport }: ReportModalProps)
           <Text textStyle="t1" fontWeight="700">
             신고하기
           </Text>
-          <IconButton w="24px" h="24px" aria-label="close" icon={<Exit />} />
+          <IconButton w="24px" h="24px" aria-label="close" icon={<Exit />} onClick={onClose} />
         </ModalHeader>
         <ModalBody display="flex" flexDir="column" h="520px" p="20px" gap="20px" alignItems="flex-start">
-          <VStack gap="12px">
+          <VStack gap="12px" align="flex-start">
             <Text textStyle="body" fontWeight="700">
               신고사유
             </Text>
             <CheckboxGroup
+              colorScheme="main"
               onChange={(v) => {
-                if (Array.isArray(v)) {
-                  setOptionType('ABUSE');
-                  if (v.includes('OTHER')) {
-                    setOptionType('OTHER');
-                  }
-                }
-                if (typeof v === 'number') {
-                  setOptionType('ABUSE');
-                }
-                if (typeof v === 'string') {
+                if (v.includes('OTHER')) {
                   setOptionType('OTHER');
+                }
+                if (!v.includes('OTHER') && v.length > 0) {
+                  setOptionType('ABUSE');
+                }
+                if (v.length === 0) {
+                  setOptionType(null);
                 }
               }}
             >
-              <Checkbox size="md" colorScheme="main" value={0}>
+              <Checkbox size="md" value="0">
                 욕설 및 혐오발언
               </Checkbox>
-              <Checkbox size="md" colorScheme="main" value={1}>
+              <Checkbox size="md" value="1">
                 음란하거나 성적인 발언
               </Checkbox>
-              <Checkbox size="md" colorScheme="main" value={2}>
+              <Checkbox size="md" value="2">
                 허위 발언
               </Checkbox>
-              <Checkbox size="md" colorScheme="main" value={3}>
+              <Checkbox size="md" value="3">
                 도배하는 댓글
               </Checkbox>
-              <Checkbox size="md" colorScheme="main" value={4}>
+              <Checkbox size="md" value="4">
                 홍보 및 불법광고
               </Checkbox>
-              <Checkbox size="md" colorScheme="main" value={5}>
+              <Checkbox size="md" value="5">
                 개인정보 노출
               </Checkbox>
-              <Checkbox size="md" colorScheme="main" value="OTHER">
+              <Checkbox size="md" value="OTHER">
                 기타
               </Checkbox>
             </CheckboxGroup>
           </VStack>
           <Textarea
             h="224px"
-            ref={reportCommentRef}
+            value={reportComment}
+            onChange={(e) => setReportComment(e.target.value)}
             rows={5}
+            placeholder="상세한 신고 사유를 입력해 주세요."
             _placeholder={{
               textStyle: 't2',
               fontWeight: '400',
@@ -101,23 +108,19 @@ export const ReportModal = ({ isOpen, onClose, handleReport }: ReportModalProps)
             h="48px"
             borderRadius="4px"
             p="14px 12px"
-            isActive={optionType === 'ABUSE' || (optionType === 'OTHER' && Boolean(reportCommentRef.current?.value))}
-            _active={{
-              bgColor: 'main',
-              color: 'white',
-            }}
-            cursor={
-              optionType === 'ABUSE' || (optionType === 'OTHER' && Boolean(reportCommentRef.current?.value))
-                ? 'pointer'
-                : 'not-allowed'
-            }
+            bgColor={buttonActive ? 'main' : 'gray200'}
+            fontWeight="700"
+            textStyle="t2"
+            color={buttonActive ? 'white' : 'gray500'}
+            disabled={!buttonActive}
+            cursor={buttonActive ? 'pointer' : 'not-allowed'}
             onClick={() => {
-              if (optionType !== undefined) {
+              if (optionType !== null) {
                 if (optionType === 'ABUSE') {
-                  handleReport(optionType, reportCommentRef.current?.value);
+                  handleReport(optionType, reportComment.trim());
                 }
-                if (optionType === 'OTHER' && reportCommentRef.current?.value) {
-                  handleReport(optionType, reportCommentRef.current.value);
+                if (optionType === 'OTHER' && reportComment.length > 0) {
+                  handleReport(optionType, reportComment.trim());
                 }
               }
             }}
