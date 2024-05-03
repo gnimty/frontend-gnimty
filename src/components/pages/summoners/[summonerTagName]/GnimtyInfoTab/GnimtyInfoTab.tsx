328188471ab0d461a048bd8f0fa329db25b84cea
LucetTin5 <lucettin5@gmail.com>
2024-05-03 09:35:21 +0900

    feat: gnimty user가 아닌 경우 이미지 표시

--- src/components/pages/summoners/[summonerTagName]/GnimtyInfoTab/GnimtyInfoTab.tsx
+++ src/components/pages/summoners/[summonerTagName]/GnimtyInfoTab/GnimtyInfoTab.tsx
@@ -1,8 +1,20 @@
 import { VStack } from '@chakra-ui/react';
+import Image from 'next/image';
+
+import notGnimtyImage from '@/assets/images/summoner-not-gnimty.png';
 
 export default function GnimtyInfoTab() {
   // 그님티 정보가 없는 회원에 대한 이미지와 문구 표시
-  // return ()
+  const gnimtyUser = false;
+
+  if (!gnimtyUser) {
+    return (
+      <VStack pt="120px" gap="24px">
+        <Image src={notGnimtyImage} width={160} height={160} alt="그님티 정보가 없는 회원" />
+      </VStack>
+    );
+  }
+
   return (
     <VStack w="full" gap="24px" align="flex-start">
       {/* 상태 메시지 */}
