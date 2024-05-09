c0d1aab760fdbedf6997224339092cacd420068f
Han Yeong-woo <han@yeongwoo.dev>
2024-05-09 20:17:56 +0900

    feat: basic riot oauth

--- src/components/pages/mypage/userInfo/UserInfoTab.tsx
+++ src/components/pages/mypage/userInfo/UserInfoTab.tsx
@@ -1,12 +1,19 @@
 import { Flex, HStack, RadioGroup } from '@chakra-ui/react';
 
+import useAuth from '@/apis/useAuth';
 import GoogleLoginButton from '@/components/common/buttons/GoogleLoginButton';
 import KakaoLoginButton from '@/components/common/buttons/KakaoLoginButton';
 import ContentsContainer from '@/components/pages/mypage/ContentsContainer';
 import UserRiotAccountRadio from '@/components/pages/mypage/userInfo/UserRiotAccountRadio';
 
 export default function UserInfoTab() {
-  const _testAccount = ['잔나긔여워', '힝구리퐁퐁퐁퐁'];
+  const { data, status, isAuthenticated } = useAuth();
+
+  // TODO: 후에 status 체크는 삭제
+  if (!isAuthenticated || status !== 'success') {
+    return;
+  }
+
   return (
     <Flex direction="column" w="full" gap="24px">
       <ContentsContainer title="소셜 로그인">
@@ -18,11 +25,11 @@ export default function UserInfoTab() {
       <ContentsContainer title="계정 연동">
         <RadioGroup w="full">
           <Flex direction="column" gap="8px">
-            {_testAccount.map((account, index) => (
+            {data.data.riotDependentInfo.riotAccounts.map((account) => (
               <UserRiotAccountRadio
-                key={index}
-                radioProps={{ value: account }}
-                riotAccountInfo={{ id: 0, nickname: account }}
+                key={account.id}
+                radioProps={{ value: account.name }}
+                riotAccountInfo={{ id: account.id, nickname: account.name }}
               />
             ))}
             <UserRiotAccountRadio key="_empty" radioProps={{ value: '_custom' }} />
