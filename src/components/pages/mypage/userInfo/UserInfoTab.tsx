d228c7b5941b4d477b42ce54d68f5ff461031f8c
Han Yeong-woo <han@yeongwoo.dev>
2024-05-12 19:33:47 +0900

    feat: change main riot account (#204)

--- src/components/pages/mypage/userInfo/UserInfoTab.tsx
+++ src/components/pages/mypage/userInfo/UserInfoTab.tsx
@@ -1,6 +1,8 @@
 import { Flex, HStack, RadioGroup } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 import useAuth from '@/apis/useAuth';
+import useChangeMainRiotAccount from '@/apis/useChangeMainRiotAccount';
 import GoogleLoginButton from '@/components/common/buttons/GoogleLoginButton';
 import KakaoLoginButton from '@/components/common/buttons/KakaoLoginButton';
 import ContentsContainer from '@/components/pages/mypage/ContentsContainer';
@@ -8,11 +10,15 @@ import UserRiotAccountRadio from '@/components/pages/mypage/userInfo/UserRiotAcc
 
 export default function UserInfoTab() {
   const { data, status, isAuthenticated } = useAuth();
+  const { changeMainRiotAccount } = useChangeMainRiotAccount();
+  const router = useRouter();
 
   // TODO: 후에 status 체크는 삭제
   if (!isAuthenticated || status !== 'success') {
     return;
   }
+
+  const mainAccount = data.data.riotDependentInfo.riotAccounts.find((account) => account.isMain);
 
   return (
     <Flex direction="column" w="full" gap="24px">
@@ -23,12 +29,30 @@ export default function UserInfoTab() {
         </HStack>
       </ContentsContainer>
       <ContentsContainer title="계정 연동">
-        <RadioGroup w="full">
+        <RadioGroup
+          w="full"
+          value={mainAccount?.id.toString()}
+          onChange={(toChangeId) => {
+            if (confirm('대표 소환사를 변경하시겠습니까?')) {
+              changeMainRiotAccount(
+                { id: parseInt(toChangeId, 10) },
+                {
+                  onSuccess() {
+                    router.reload();
+                  },
+                  onError() {
+                    alert('대표 소환사 변경을 실패했습니다.');
+                  },
+                },
+              );
+            }
+          }}
+        >
           <Flex direction="column" gap="8px">
             {data.data.riotDependentInfo.riotAccounts.map((account) => (
               <UserRiotAccountRadio
                 key={account.id}
-                radioProps={{ value: account.name }}
+                radioProps={{ value: account.id.toString() }}
                 riotAccountInfo={{ id: account.id, nickname: account.name }}
               />
             ))}
