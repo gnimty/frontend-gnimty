fa1e7f800073278bb0481235c340fcb40e59191a
LucetTin5 <lucettin5@gmail.com>
2024-05-13 14:15:36 +0900

    fix: password regex 수정

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,4 +1,4 @@
 export const emailRegex = /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-z A-Z]{2,7}$/;
-export const passwordRegex = /^(?=.[a-zA-Z])(?=.\d)(?=.[!"#$%&'()+,-./:;<=>?@[]^_{|}~]).{8,16}$/;
+export const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!"#$%&'()+,-./:;<=>?@[\]^_{|}~]).{8,16}$/;
 
 export const authCodeRegex = /^[A-Z0-9]{6}$/;
