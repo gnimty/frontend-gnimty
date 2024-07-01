f5f858f35da5158f7db3ffcb8ef2623a828015c8
LucetTin5 <Lucettin5@gmail.com>
2024-07-01 22:21:20 +0900

    fix: reset font import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/reset.css';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import createCache from '@emotion/cache';
-import { CacheProvider, ThemeProvider } from '@emotion/react';
+import { CacheProvider, ThemeProvider, Global } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
@@ -19,6 +19,14 @@ const emotionCache = createCache({
   key: 'css',
   stylisPlugins: [],
 });
+
+const Fonts = () => (
+  <Global
+    styles={`
+    @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.min.css');
+  `}
+  />
+);
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -52,6 +60,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <CacheProvider value={emotionCache}>
           <ChakraBaseProvider theme={chakraTheme} resetCSS={false}>
             <ThemeProvider theme={emotionTheme}>
+              <Fonts />
               <ChatContextProvider>
                 <BaseLayout>
                   <Component {...pageProps} />
